845647b7a4e639647992e7c391d88e29d65e4b6a
nguyenhuuluan434 <nguyenhuuluan434@gmail.com>
2020-01-20 10:43:32 +0700

    init project

---
+++ src/test/java/com/jpa/specification/jpa/specification/JpaSpecificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.specification.jpa.specification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSpecificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
